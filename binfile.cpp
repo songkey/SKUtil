00a5159030b83fe5d0c0593b7f2f00272e4a33bd
songkey <songkey@outlook.com>
2016-04-19 17:57:46 +0800

    first utils about binfile proc

---
+++ binfile.cpp
@@ -0,0 +1,68 @@
+nclude "binfile.h"
+
+namespace sk{
+
+  binFR::binFR(const std::string& filePath){
+    fr_.open(filePath.c_str(), std::ios::binary);
+  }
+
+  binFR::~binFR()
+  {
+    if (fr_.is_open()){
+      fr_.close();
+    }
+  }
+
+  void binFR::close(){
+    if (fr_.is_open()){
+      fr_.close();
+    }
+  }
+
+  binFR& binFR::operator>>(std::string &str){
+    size_t str_len;
+    fr_.read((char*)&str_len, sizeof(size_t));
+    str.resize(str_len);
+    fr_.read((char*)str.data(), str_len);
+
+    return *this;
+  }
+
+  binFW::binFW(const std::string& filePath){
+    fw_.open(filePath.c_str(), std::ios::binary);
+  }
+
+  binFW::~binFW(){
+    if (fw_.is_open()){
+      fw_.close();
+    }
+  }
+
+  void binFW::close(){
+    if (fw_.is_open()){
+      fw_.close();
+    }
+  }
+
+  void binFW::flush(){
+    if (fw_.is_open())
+    {
+      fw_.flush();
+    }
+  }
+
+  binFW& binFW::operator<<(const std::string& str){
+    rsize_t str_len = str.length();
+    fw_.write((char*)&str_len, sizeof(size_t));
+    fw_.write((char*)str.data(), str_len);
+
+    return *this;
+  }
+
+  binFW& binFW::operator<<(const char* c_str){
+    this->operator<<(std::string(c_str));
+
+    return *this;
+  }
+}
+
---
+++ binfile.h
@@ -0,0 +1,77 @@
+"global_members_lista once
+#include <string>
+#include <fstream>
+#include <vector>
+
+namespace sk{
+
+  class binFR
+  {
+  public:
+
+    explicit binFR(const std::string&);
+    ~binFR();
+
+    void close();
+
+    template<typename Dtype>
+    binFR& operator>>(std::vector<Dtype> &vec){
+      size_t vec_len;
+      fr_.read((char*)&vec_len, sizeof(size_t));
+      vec.resize(vec_len);
+      fr_.read((char*)vec.data(), sizeof(Dtype)* vec_len);
+
+      return *this;
+    }
+
+    template<typename Dtype>
+    binFR& operator>>(Dtype &item){
+      fr_.read((char*)&item, sizeof(Dtype));
+      return *this;
+    }
+
+    binFR& operator>>(std::string&);
+
+  private:
+
+    binFR();
+    std::ifstream fr_;
+
+  };
+
+  class binFW
+  {
+  public:
+
+    explicit binFW(const std::string&);
+    ~binFW();
+
+    void close();
+    void flush();
+
+    template<typename Dtype>
+    binFW& operator<<(const std::vector<Dtype> &vec){
+      rsize_t vec_len = vec.size();
+      fw_.write((char*)&vec_len, sizeof(size_t));
+      fw_.write((char*)vec.data(), sizeof(Dtype)* vec_len);
+
+      return *this;
+    }
+
+    template<typename Dtype>
+    binFW& operator<<(Dtype item){
+      fw_.write((char*)&item, sizeof(item));
+
+      return *this;
+    }
+
+    binFW& operator<<(const std::string&);
+    binFW& operator<<(const char*);
+
+  private:
+
+    binFW();
+    std::ofstream fw_;
+
+  };
+}
